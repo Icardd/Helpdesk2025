99803c0247d00373ee12e924e51eb55a1039f959
Icardd <igorcardoso258k@gmail.com>
2025-05-24 00:31:32 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     # Configuração do banco de dados
-    SQLALCHEMY_DATABASE_URI = os.getenv("mysql+pymysql://root:STPRHqlKEeSoHZmycYqnXAceqZgIcXgR@mysql.railway.internal:3306/railway")
+    SQLALCHEMY_DATABASE_URI = os.getenv("mysql+pymysql://root:IEciefERnwCWjYtLDMLiqlTdDqruzEnS@mysql.railway.internal:3306/railway")
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Desativando o monitoramento de modificações do SQLAlchemy
     
     # Chave secreta para proteger a sessão do Flask
